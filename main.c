d2b0f74e1a104c8d6d5e025ca3345d121a744945
danya-chernikov <dchernik@proton.me>
2025-09-19 14:37:35 +0200

    Some junk files were deleted. The invalid call to bzero() was corrected

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dchernik <dchernik@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 16:50:38 by dchernik          #+#    #+#             */
-/*   Updated: 2025/09/19 15:16:56 by dchernik         ###   ########.fr       */
+/*   Updated: 2025/09/19 14:37:19 by dchernik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ int	main(int argc, char *argv[])
 {
 	t_game_data	gdata;
 
-	ft_bzero(&gdata.map, sizeof (t_game_data));
 	if (argc != 2)
 	{
 		write(STDERR_FILENO, MAP_PATH_ERR_MSG, ft_strlen(MAP_PATH_ERR_MSG));
 		exit(EXIT_FAILURE);
 	}
+	ft_bzero(&gdata, sizeof (t_game_data))
 	if (!game_init(&gdata, argv[1]))
 	{
 		exit(EXIT_FAILURE);
